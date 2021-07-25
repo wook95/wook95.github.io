14e43919e718d8ebd6f48b2355b7b6fe69616290
wook95 <uclee95@gmail.com>
2021-07-25 23:15:24 +0900

    Init blog project

---
+++ content/__about/resume-en.md
@@ -0,0 +1,21 @@
+---
+title: 'about'
+date: 2019-1-27 16:21:13
+lang: 'en'
+---
+
+<!-- <div class="myName">
+    <strong style="font-size:30px;">이욱창</strong>
+    <p style="font-size:14px; color:#999">프론트엔드 개발자 지망생</p>
+</div> 
+<hr></hr>-->
+
+# 안녕하세요! <br> 개발자 지망생 이욱창입니다😋
+
+<div align="center">
+
+성장하고싶습니다.
+
+_Thank you for reading my resume. If you want to contact me, Please send me an email._
+
+</div>
---
+++ gatsby-meta-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: `Wook.log`,
+  description: `Blog posted about ...`,
+  author: `[Wookchang Lee]`,
+  introduction: `개발자 지망생 이욱창입니다.`,
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `https://github.com/wook95`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
