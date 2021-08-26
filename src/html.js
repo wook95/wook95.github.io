798adc8de3a1d134d3fea99f5677dd3fe9b6443b
wook95 <uclee95@gmail.com>
2021-08-26 18:19:23 +0900

    Fix: google analytics

--- src/html.js
+++ src/html.js
@@ -11,7 +11,17 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-0DCYCME6NG"
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-0DCYCME6NG"></script>
+      <script
+  dangerouslySetInnerHTML={{
+    __html: `
+    window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0DCYCME6NG');
+  `,
+  }}
 />
         {props.headComponents}
       </head>
